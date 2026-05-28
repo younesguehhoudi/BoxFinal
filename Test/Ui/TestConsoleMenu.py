47dce0d903330ea4cfef02eebf75968e10fd0bf7
younesguehhoudi <younesguehhoudi@github.com>
2026-05-28 18:38:59 +0200

    test(ui) : unit test for ConsoleMenu.py

--- Test/Ui/TestConsoleMenu.py
+++ Test/Ui/TestConsoleMenu.py
@@ -1,5 +1,6 @@
 #%%
 from pathlib import Path
+import builtins
 import sys
 
 
@@ -9,24 +10,66 @@ if str(PROJECT_ROOT) not in sys.path:
 
 from Ui.ConsoleMenu import ConsoleMenu
 
-class TestConsoleMenu :
+
+class TestConsoleMenu:
 
     def test_display_title(self):
         menu = ConsoleMenu()
         menu.display_title()
-    
+
+    def test_display_options(self):
+        menu = ConsoleMenu()
+        menu.display_options()
+
+    def test_ask_choice(self):
+        menu = ConsoleMenu()
+        original_input = builtins.input
+        builtins.input = lambda prompt: "3"
+        try:
+            choice = menu.ask_choice()
+        finally:
+            builtins.input = original_input
+
+        print(choice)
+
+    def test_login_ui(self):
+        menu = ConsoleMenu()
+        menu.login_ui()
+
+    def test_add_place_ui(self):
+        menu = ConsoleMenu()
+        menu.add_place_ui()
+
+    def test_display_places_ui(self):
+        menu = ConsoleMenu()
+        menu.display_places_ui()
+
+    def test_display_tour_ui(self):
+        menu = ConsoleMenu()
+        menu.display_tour_ui()
+
     def test_process_choice(self):
         menu = ConsoleMenu()
-       # menu.process_choice(choice="1")
+        menu.process_choice("1")
+        menu.process_choice("2")
+        menu.process_choice("3")
+        menu.process_choice("4")
 
-        for i in range(1,5):
-            chaine = str(i)
-            menu.process_choice(chaine)
-        
+    def test_process_choice_invalid(self):
+        menu = ConsoleMenu()
+        menu.process_choice("0")
+
 
 if __name__ == "__main__":
     test_menu = TestConsoleMenu()
     test_menu.test_display_title()
-    test_ConsoleMenu = TestConsoleMenu()
+    test_menu.test_display_options()
+    test_menu.test_ask_choice()
+    test_menu.test_login_ui()
+    test_menu.test_add_place_ui()
+    test_menu.test_display_places_ui()
+    test_menu.test_display_tour_ui()
     test_menu.test_process_choice()
+    test_menu.test_process_choice_invalid()
+
 # %%
