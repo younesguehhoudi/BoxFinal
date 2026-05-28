b2e76c3f1eb3c1e2d7a1faf4d8efdb40141e8702
Noeloisel22 <noe.loisel102@gmail.com>
2026-05-28 15:31:58 +0200

    feat:(logic) creating class Tour and Places for OPP

--- Logic/Tour.py
+++ Logic/Tour.py
@@ -1,9 +1,7 @@
-from Logic.Place import Place
-
 class Tour:
-    def __init__(self, name, places, visibility):
+    def __init__(self,name : chr, places : list, visibility : chr):
         self.name = name
-        self.places = places        
+        self.places = places
         self.visibility = visibility
         self.total_distance = 0.0
-        self.share_token = None
+        self.share_token = chr
